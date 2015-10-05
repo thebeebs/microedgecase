68209d6d25c3867bf75e4eddfcb782de8e2df035
Lilian Kasem <liliankasem@gmail.com>
2015-10-05 16:13:37 +0100

    Initial UAP app draft
    Initial commit of a UAP application which just takes in a URL and
    returns results from the API.

---
+++ EdgeCasesApp/EdgeCasesApp/EdgeCaseModel.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdgeCasesApp
+{
+    class EdgeCaseModel
+    {
+
+        public async static Task<RootObject> GetResults(string url)
+        {
+            var http = new HttpClient();
+            var apiUrl = String.Format("http://edge.azurewebsites.net/api/v2/scan?url=http://{0}", url);
+            var response = await http.GetAsync(apiUrl);
+            var result = await response.Content.ReadAsStringAsync();
+            var serializer = new DataContractJsonSerializer(typeof(RootObject));
+
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(result));
+            var data = (RootObject)serializer.ReadObject(ms);
+
+            return data;
+        }
+
+    }
+
+    [DataContract]
+    public class Url
+    {
+        [DataMember]
+        public string uri { get; set; }
+    }
+
+    [DataContract]
+    public class Datum
+    {
+        [DataMember]
+        public bool passed { get; set; }
+        [DataMember]
+        public string pattern { get; set; }
+        [DataMember]
+        public int lineNumber { get; set; }
+        [DataMember]
+        public string url { get; set; }
+    }
+
+    [DataContract]
+    public class Javascript
+    {
+        [DataMember]
+        public bool passed { get; set; }
+        [DataMember]
+        public List<Datum> data { get; set; }
+    }
+
+    [DataContract]
+    public class Comments
+    {
+        [DataMember]
+        public bool passed { get; set; }
+    }
+
+    [DataContract]
+    public class Data
+    {
+        [DataMember]
+        public Javascript javascript { get; set; }
+        [DataMember]
+        public Comments comments { get; set; }
+    }
+
+    [DataContract]
+    public class BrowserDetection
+    {
+        [DataMember]
+        public string testName { get; set; }
+        [DataMember]
+        public bool passed { get; set; }
+        [DataMember]
+        public Data data { get; set; }
+    }
+
+    [DataContract]
+    public class Cssprefixes
+    {
+        [DataMember]
+        public string testName { get; set; }
+        [DataMember]
+        public bool passed { get; set; }
+        [DataMember]
+        public List<object> data { get; set; }
+    }
+
+    [DataContract]
+    public class Data2
+    {
+        [DataMember]
+        public int lineNumber { get; set; }
+        [DataMember]
+        public List<string> mode { get; set; }
+    }
+
+    [DataContract]
+    public class Edge
+    {
+        [DataMember]
+        public string testName { get; set; }
+        [DataMember]
+        public bool passed { get; set; }
+        [DataMember]
+        public Data2 data { get; set; }
+    }
+
+    [DataContract]
+    public class Jslibs
+    {
+        [DataMember]
+        public string testName { get; set; }
+        [DataMember]
+        public string url { get; set; }
+        [DataMember]
+        public bool passed { get; set; }
+        [DataMember]
+        public List<object> data { get; set; }
+    }
+
+    [DataContract]
+    public class Pluginfree
+    {
+        [DataMember]
+        public string testName { get; set; }
+        [DataMember]
+        public bool passed { get; set; }
+        [DataMember]
+        public List<object> data { get; set; }
+    }
+
+    [DataContract]
+    public class Datum2
+    {
+        [DataMember]
+        public string element { get; set; }
+        [DataMember]
+        public double threshold { get; set; }
+        [DataMember]
+        public int edgeCount { get; set; }
+        [DataMember]
+        public int chromeCount { get; set; }
+        [DataMember]
+        public bool passed { get; set; }
+    }
+
+    [DataContract]
+    public class Markup
+    {
+        [DataMember]
+        public string testName { get; set; }
+        [DataMember]
+        public bool passed { get; set; }
+        [DataMember]
+        public List<Datum2> data { get; set; }
+    }
+
+    [DataContract]
+    public class Results
+    {
+        [DataMember]
+        public BrowserDetection browserDetection { get; set; }
+        [DataMember]
+        public Cssprefixes cssprefixes { get; set; }
+        [DataMember]
+        public Edge edge { get; set; }
+        [DataMember]
+        public Jslibs jslibs { get; set; }
+        [DataMember]
+        public Pluginfree pluginfree { get; set; }
+        [DataMember]
+        public Markup markup { get; set; }
+    }
+
+    [DataContract]
+    public class RootObject
+    {
+        [DataMember]
+        public Url url { get; set; }
+        [DataMember]
+        public double processTime { get; set; }
+        [DataMember]
+        public string machine { get; set; }
+        [DataMember]
+        public Results results { get; set; }
+    }
+}
---
+++ EdgeCasesApp/EdgeCasesApp/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace EdgeCasesApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+
+            var url = textBox.Text;
+            RootObject edgeCase = await EdgeCaseModel.GetResults(url);
+
+            textResult_browserDetection.Text = "Test: " + edgeCase.results.browserDetection.testName + " \nPassed?: " + edgeCase.results.browserDetection.passed.ToString();
+            textResult_markup.Text = "Test: " + edgeCase.results.markup.testName + " \nPassed?: " + edgeCase.results.markup.passed.ToString();
+            textResult_pluginFree.Text = "Test: " + edgeCase.results.pluginfree.testName + " \nPassed?: " + edgeCase.results.pluginfree.passed.ToString();
+            textResult_jsLibs.Text = "Test: " + edgeCase.results.jslibs.testName + " \nPassed?: " + edgeCase.results.jslibs.passed.ToString();
+            textResult_edge.Text = "Test: " + edgeCase.results.edge.testName + " \nPassed?: " + edgeCase.results.edge.passed.ToString();
+            textResult_css.Text = "Test: " + edgeCase.results.cssprefixes.testName + " \nPassed?: " + edgeCase.results.cssprefixes.passed.ToString();
+
+        }
+    }
+}
